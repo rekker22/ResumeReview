ebbd721df2d4cc707e2aadd25cfeee3dab75e7eb
subhasish Mandal <sm8017797063@gmail.com>
2021-11-20 14:10:00 +0530

    Enabled Error page for Prod Troubleshoot

--- ResumeReview/Startup.cs
+++ ResumeReview/Startup.cs
@@ -81,7 +81,7 @@ namespace ResumeReview
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (!env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
