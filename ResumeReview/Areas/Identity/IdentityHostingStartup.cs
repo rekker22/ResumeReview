07d3dce5a3469f6a565b1a7e168de29a88b37260
subhasish Mandal <sm8017797063@gmail.com>
2021-11-20 13:38:57 +0530

    Rectified conn string

--- ResumeReview/Areas/Identity/IdentityHostingStartup.cs
+++ ResumeReview/Areas/Identity/IdentityHostingStartup.cs
@@ -46,9 +46,9 @@ namespace ResumeReview.Areas.Identity
                     services.AddDbContextPool<ApplicationDbContext>(options =>
                     {
                         // Use connection string provided at runtime by Heroku.
-                        //var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-                        var connUrl = context.Configuration.GetConnectionString("ProductionConnection");
+                        //var connUrl = context.Configuration.GetConnectionString("ProductionConnection");
                         // Parse connection URL to connection string for Npgsql
                         connUrl = connUrl.Replace("postgres://", string.Empty);
                         var pgUserPass = connUrl.Split("@")[0];
--- ResumeReview/Startup.cs
+++ ResumeReview/Startup.cs
@@ -49,9 +49,9 @@ namespace ResumeReview
                 services.AddDbContextPool<ApplicationDbContext>(options =>
                 {
                     // Use connection string provided at runtime by Heroku.
-                    //var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-                    var connUrl = Configuration.GetConnectionString("ProductionConnection");
+                   // var connUrl = Configuration.GetConnectionString("ProductionConnection");
                     // Parse connection URL to connection string for Npgsql
                     connUrl = connUrl.Replace("postgres://", string.Empty);
                     var pgUserPass = connUrl.Split("@")[0];
